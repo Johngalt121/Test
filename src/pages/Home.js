cbb114c233c54ec574fbf9d1322b5ae1727b1d14
vishal <vishal.kandukuri>
2023-11-23 17:13:39 +0530

    first commit

---
+++ src/pages/Home.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Button } from 'reactstrap';
+const Home =()=>{
+    return (
+        <Button color="primary" className="btn-label"> <i className="ri-user-smile-line label-icon align-middle fs-16 me-2"></i> Primary </Button>
+        )
+}
+
+
+export default Home
